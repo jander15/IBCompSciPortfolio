743cc48e1252650ac429775f821e050799023af4
jander15 <45211598+jander15@users.noreply.github.com>
2023-01-10 12:12:34 -0700

    added snake game

---
+++ src/Snake/objects/Apple.java
@@ -0,0 +1,23 @@
+package Snake.objects;
+
+import Snake.snakegame.SnakeGame;
+
+public class Apple {
+
+    SnakeGame main;
+
+    public int posX;
+    public int posY;
+
+    public Apple(int startX, int startY) {
+        posX = startX;
+        posY = startY;
+
+    }
+
+    @SuppressWarnings("static-access")
+    public void setRandomPosition() {
+        posX = (int) (Math.random() * main.WIDTH);
+        posY = (int) (Math.random() * main.HEIGHT);
+    }
+}
